0264ff12d739ce7c5f9160d2842861e53225033f
Stefan Cloudt <s.d.cloudt@student.tue.nl>
2016-03-16 15:26:17 +0100

    Solved InitialState to PausedState problem while maintaining finite automaton from spec

--- Phases/Software Design/States/src/states/InitialState.java
+++ Phases/Software Design/States/src/states/InitialState.java
@@ -4,6 +4,7 @@ import sorter.Main;
 
 public class InitialState extends State {
 	private boolean calibrationStarted = false;
+	private boolean pressed = false;
 
 	@Override
 	public State nextState(Main m) {
@@ -16,6 +17,17 @@ public class InitialState extends State {
 		else if(m.touchSensor.isPressed()){
 			m.motor.stop();
 			m.motor.reset();
+//			m.setPaused(false);
+//			m.setReset(false);
+//			return new PausedState(m);
+		}
+		
+		
+		if(m.spButton.isDown()){
+			this.pressed = false;
+		}
+		
+		if(this.pressed && m.spButton.isUp()){
 			m.setPaused(false);
 			m.setReset(false);
 			return new PausedState(m);
