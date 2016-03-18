ee56f6d231a6d274d6ce0a527fe612b1d74b6194
unknown <s158208@s158208.campus.tue.nl>
2016-03-18 15:57:48 +0100

    Test peripheral connection

--- Phases/Software Design/States/src/sorter/AbstractMain.java
+++ Phases/Software Design/States/src/sorter/AbstractMain.java
@@ -2,18 +2,21 @@ package sorter;
 
 import error.AbortButtonError;
 import error.BatteryWarning;
+import error.PeripheralConnectionLostError;
 import error.SofwareError;
+import lejos.hardware.DeviceException;
 import lejos.hardware.Key;
+import lejos.hardware.device.DeviceIdentifier;
+import lejos.hardware.port.SensorPort;
+import peripherals.Battery;
 import peripherals.ColorSensor;
 import peripherals.Display;
 import peripherals.GyroSensor;
 import peripherals.Motor;
 import peripherals.TouchSensor;
 import states.AbortState;
-import states.ModeSelectionState;
 import states.State;
 import states.WarningState;
-import peripherals.Battery;;
 
 public abstract class AbstractMain {
 	public static final int MOTOR_TURN_STEP = 		120;
@@ -73,8 +76,14 @@ public abstract class AbstractMain {
 	public void run(){
 		try {
 			while(true){
+				testPeripherals();
 				cycle();
 			}
+		}
+		catch(DeviceException e) {
+			currentState = new AbortState(new PeripheralConnectionLostError(), this);
+			currentState.displayUpdate(this);
+			run();
 		}
 		catch(Exception e){
 			currentState = new AbortState(new SofwareError(this), this);
@@ -84,6 +93,7 @@ public abstract class AbstractMain {
 	}
 	
 	public void cycle(){
+		
 		//check buttons
 		if(aButton.isDown() && !(currentState instanceof AbortState)){
 			currentState = new AbortState(new AbortButtonError(), this);
@@ -103,6 +113,19 @@ public abstract class AbstractMain {
 		currentState.displayUpdate(this);
 		State newState = currentState.nextState(this);
 		currentState = newState;
+	}
+	
+	public void testPeripherals() throws DeviceException {
+		try {
+			System.out.println("TEST START");
+			DeviceIdentifier d = new DeviceIdentifier(SensorPort.S1);
+			d.getDeviceType();
+			d.close();
+			System.out.println("TEST PASSED");
+		} catch(Exception e) {
+			System.out.println("TEST FAILED");
+			throw new DeviceException();
+		}
 	}
 	
 	public void variableReset(){
