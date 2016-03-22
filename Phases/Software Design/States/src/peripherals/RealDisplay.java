3cbe522c043f1124f87adeadee7dbdac20096294
Daan de Graaf <daandegraaf9@gmail.com>
2016-03-22 12:57:31 +0100

    Refix the time and success rate

--- Phases/Software Design/States/src/peripherals/RealDisplay.java
+++ Phases/Software Design/States/src/peripherals/RealDisplay.java
@@ -69,6 +69,7 @@ public class RealDisplay implements Display {
 		if(!isReady()) return;
 		g.setFont(Font.getDefaultFont());
 		g.drawString("Success: "+(chance)*100 + "%", SW/2, SH-20, g.BASELINE|g.HCENTER, true);
+		g.refresh();
 	}
 	
 	@SuppressWarnings("static-access")
@@ -76,6 +77,7 @@ public class RealDisplay implements Display {
 		if(!isReady()) return;
 		g.setFont(Font.getDefaultFont());
 		g.drawString("Time: " + t + "ms", SW/2, SH-30, g.BASELINE|g.HCENTER, true);
+		g.refresh();
 	}
 	
 	private boolean isReady() {
